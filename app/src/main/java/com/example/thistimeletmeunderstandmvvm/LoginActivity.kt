4cac65d7ae6ebd5218346c038793cc95985046ea
Maciej Skrabski <maciej.skrabski@gmail.com>
2020-05-24 14:10:09 +0200

    input validation

--- app/src/main/java/com/example/thistimeletmeunderstandmvvm/LoginActivity.kt
+++ app/src/main/java/com/example/thistimeletmeunderstandmvvm/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.thistimeletmeunderstandmvvm
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "Login Activity"
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +22,40 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(
                 AppCompatDelegate.MODE_NIGHT_YES);
 
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+        Log.d(TAG, "$currentUser")
+
         register.setOnClickListener {
             startActivity(Intent(this@MainActivity, RegisterActivity::class.java ))
         }
+
+        loginl.setOnClickListener {
+            val mail = emaill.text.toString().trim()
+            val password = passwordl.text.toString().trim()
+
+            if ( mail.isEmpty() ) {
+                emaill.error = "Email shall not be empty!"
+                emaill.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                emaill.error = "Actual email is required!"
+                emaill.requestFocus()
+                return@setOnClickListener
+            }
+
+            if ( password.isEmpty() ) {
+                passwordl.error = "Password shall not be empty!"
+                passwordl.requestFocus()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
     }
 }
--- app/src/main/java/com/example/thistimeletmeunderstandmvvm/RegisterActivity.kt
+++ app/src/main/java/com/example/thistimeletmeunderstandmvvm/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.thistimeletmeunderstandmvvm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,6 +12,33 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        register_button.setOnClickListener {
+            val mail = email.text.toString().trim()
+            val pass = password.text.toString().trim()
+
+            if ( mail.isEmpty() ) {
+                email.error = "Email shall not be empty!"
+                email.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+                email.error = "Actual email is required!"
+                email.requestFocus()
+                return@setOnClickListener
+            }
+
+            if ( pass.isEmpty() ) {
+                password.error = "Password shall not be empty!"
+                password.requestFocus()
+                return@setOnClickListener
+            }
+
+        }
+
+
+
         register_tv.setOnClickListener { startActivity(Intent(this@RegisterActivity, MainActivity::class.java )) }
+
     }
 }
