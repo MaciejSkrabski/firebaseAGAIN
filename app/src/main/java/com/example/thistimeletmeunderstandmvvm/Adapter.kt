30dce8cf1baa76feeff8bfffd50b4a8fa0f854f5
Maciej Skrabski <maciej.skrabski@gmail.com>
2020-05-25 08:56:23 +0200

    Edit navigation

--- app/src/main/java/com/example/thistimeletmeunderstandmvvm/Adapter.kt
+++ app/src/main/java/com/example/thistimeletmeunderstandmvvm/Adapter.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -29,6 +32,14 @@ class BooksAdapter: RecyclerView.Adapter<BooksAdapter.BooksViewHolder>(){
         holder.title.text = bookList[holder.adapterPosition].title
         holder.author.text = bookList[holder.adapterPosition].author
         holder.read.text = bookList[holder.adapterPosition].read.toString()
+
+        holder.itemView.setOnClickListener{
+            Log.d("CLICK", "RECYCLERVIEW ${bookList[holder.adapterPosition].comment}")
+            val nav = Navigation.findNavController(it)
+            nav.navigate(R.id.editRecord)
+        }
+
+
     }
 
     fun setbooks(newBooks:List<BookRow>){
@@ -52,63 +63,9 @@ class BooksAdapter: RecyclerView.Adapter<BooksAdapter.BooksViewHolder>(){
         val author = view.findViewById(R.id.book_author) as TextView
         val read = view.findViewById(R.id.read) as TextView
     }
-
-
-    /*
 }
+/*class ItemListener() {
+    fun onClick(night: BookRow) =
+}*/
 
-class FilmsAdapter (private val dataArray: ArrayList<FilmRow>):
-    RecyclerView.Adapter<FilmsAdapter.MyViewHolder>(){
 
-    override fun onCreateViewHolder (parent: ViewGroup, viewType: Int)
-            : MyViewHolder {
-        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
-        val view: View = inflater.inflate(R.layout.film_row, parent, false)
-        return MyViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return dataArray.size
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position:Int) {
-        holder.title.text = dataArray[holder.adapterPosition].title
-        holder.director.text = dataArray[holder.adapterPosition].director
-        holder.watched.text = dataArray[holder.adapterPosition].watched.toString()
-    }
-
-    inner class MyViewHolder(view: View):RecyclerView.ViewHolder(view) {
-        val title = view.findViewById(R.id.film_title) as TextView
-        val director = view.findViewById(R.id.film_director) as TextView
-        val watched = view.findViewById(R.id.watched) as TextView
-    }
-} */
-/*
-class GamesAdapter (private val dataArray: ArrayList<GameRow>):
-    RecyclerView.Adapter<GamesAdapter.MyViewHolder>(){
-
-    override fun onCreateViewHolder (parent: ViewGroup, viewType: Int)
-            : MyViewHolder {
-        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
-        val view: View = inflater.inflate(R.layout.game_row, parent, false)
-        return MyViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return dataArray.size
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position:Int) {
-        holder.title.text = dataArray[holder.adapterPosition].title
-        holder.studio.text = dataArray[holder.adapterPosition].studio
-        holder.played.text = dataArray[holder.adapterPosition].played.toString()
-    }
-
-    inner class MyViewHolder(view: View):RecyclerView.ViewHolder(view) {
-        val title = view.findViewById(R.id.game_title) as TextView
-        val studio = view.findViewById(R.id.game_studio) as TextView
-        val played = view.findViewById(R.id.played) as TextView
-    }
-
- */
-}
